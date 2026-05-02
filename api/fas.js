175c2332cdc05bd316260ebba4f9f4d1b672fd93
Beula28 <hepshibabeula28@gmail.com>
2026-05-02 18:27:22 +0530

    Update fas.js

--- api/fas.js
+++ api/fas.js
@@ -39,7 +39,7 @@ export default async function handler(req, res) {
     }
 
     // 🔹 Stable session (IP + token)
-    const sessionId = makeSessionId(`${clientip}|${tok}`);
+    const sessionId = makeSessionId(`${clientip}`);
 
     // 🔹 Store client
     const clientRecord = {
